fc79a208f134aaa5d263e97558b4a16ec6846881
Nicolas Albornoz <nicolasalbornoz@MacBook-Pro-de-Nicolas.local>
2022-09-19 23:52:42 -0300

    first commit5

--- controllers/controllersConsultas.js
+++ controllers/controllersConsultas.js
@@ -23,25 +23,3 @@ module.exports = {
 };
 
 
-const conexion = require ('../db/db');
-
-
-
- consultas = (req, res) => {
-     let sql = 'SELECT * FROM productos ';
-
-     conexion.query(sql,  (err, result) => {
-         if (err) throw err;
-    res.render('consultas', {
-        titulo: 'Listado de Producto',
-        result
-    })
-    })
-}
-
-
-
-module.exports = {
-    consultas
-};
-
